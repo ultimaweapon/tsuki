aca1eb7b9f37b286afe5476170308b570e689131
Putta Khunchalee <ultimaweapon@outlook.com>
2025-09-05 16:58:51 +0700

    Makes snprintf public

--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -3,5 +3,5 @@ pub use libc::*;
 #[cfg(windows)]
 unsafe extern "C-unwind" {
     #[link_name = "tsuki_snprintf"]
-    fn snprintf(buffer: *mut c_char, count: usize, format: *const c_char, ...) -> c_int;
+    pub fn snprintf(buffer: *mut c_char, count: usize, format: *const c_char, ...) -> c_int;
 }
